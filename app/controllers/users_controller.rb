575da2e5343660d115395e70840ada6854064a99
Yumiko Tezuka <ytezuka5@gmail.com>
2016-11-12 06:26:51 +0000

    add posting microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,32 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   
-  def show # 追加
-   @user = User.find(params[:id])
+  def edit
+    @user = User.find(params[:id])
+    if (current_user != @user)
+      redirect_to root_path
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if (current_user != @user)
+      redirect_to root_path
+    end
+    if @user.update(user_params)
+      flash[:success] = "Updated your Plofile"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def show 
+
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
+
   end
   
   def new
@@ -23,7 +48,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, 
+    params.require(:user).permit(:name, :email,:place, :password, 
     :password_confirmation)
   end
 end
