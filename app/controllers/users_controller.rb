f5fab82b9f798f3f59cfc247ce18381b3e871639
Yumiko Tezuka <ytezuka5@gmail.com>
2016-11-14 05:46:22 +0000

    add followings/followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,49 +1,54 @@
 class UsersController < ApplicationController
-    
-    def show
+   before_action :correct_user, only: [:edit, :update]
+   
+  def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.order(created_at: :desc)
   end
   
-    before_action :correct_user, only: [:edit, :update]
     
-    def edit
-    end
+  def edit
+  end
     
-    def update
+  def update
     @user = User.find(params[:id])
-    if @user.update(user_params)
-      flash[:success] = "Updated your Plofile"
+    if @user.update(user_profile)
+      flash[:success] = "Updated your Profile"
       redirect_to @user
     else
       render 'edit'
     end
   end
   
-   def new
+  def new
     @user = User.new
   end
 
- def create
+  def create
     @user = User.new(user_params)
     if @user.save
         flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-  else
+    else
       render 'new'
     end
   end
 
   private
-   def correct_user
-  @user = User.find(params[:id])
+    def correct_user
+        @user = User.find(params[:id])
   
-  unless current_user == @user
-      redirect_to root_url
-    end     
+        unless current_user == @user
+          redirect_to root_url
+        end     
   end
 
   def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+  
+  def user_profile
     params.require(:user).permit(:name, :email, :place, :password,
                                  :password_confirmation)
   end
--- db/migrate/20161112063923_create_relationships.rb
+++ db/migrate/20161112063923_create_relationships.rb
@@ -5,6 +5,8 @@ class CreateRelationships < ActiveRecord::Migration
       t.references :followed, index: true, foreign_key: true
 
       t.timestamps null: false
+      t.index [:follower_id, :followed_id], unique: true # この行を追加
+    
     end
   end
 end
