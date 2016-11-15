6b2d102897b13e00e4e5ebd284a24dbd226df581
Yumiko Tezuka <ytezuka5@gmail.com>
2016-11-15 19:39:16 +0000

    add followings/followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,17 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+def followings
+  @user = User.find(params[:id])
+  @users = @user.following_users
+end
+
+def followers
+  @user = User.find(params[:id])
+  @users = @user.follower_users
+    
+end
 
   private
     def correct_user
